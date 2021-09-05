184f6b4c265fbe3faccde9a2e64cbfc3399d454b
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-04 23:07:55 -0400

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,53 @@
 // Code EyesOnMe Component Here
+//////// MY CODE ////////
+// import React from "react";
+
+// function Eyes() {
+//     function focus(){
+//       console.log("Good!");
+//     }
+//     return <button onClick={focus}>Good!</button>
+
+//     function blur() {
+//         console.log("Hey! Eyes on me!");
+//     }
+//     return <button onClick={blur}>Hey! Eyes on me!</button>
+// }
+
+// export default Eyes
+
+
+//////// SOLUTION ////////
+import React from "react";
+
+function Eyes(){
+    function focus(){
+        console.log("Good!");
+    }
+
+    function blur(){
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+    <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    );
+}
+
+
+export default Eyes
+
+///// NOTES //////
+// 1. didn't carefully read that the event handlers were legit FOCUS AND BLUR
+// 2. had the right idea of how to make two functions but was trying to use two return statements 
+// 3. GREAT JOB!
+
+
+
+
+// Steps:
+// 1. In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// 2. In that component, render a button with the text 'Eyes on me'.
+// 3. On that button, add event handlers that listen for the focus and blur events.
+// 4. When the focus event fires, use console.log to print out the text 'Good!'.
+// 5. When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,49 @@
 // Code Keypad Component Here
+//////// MY CODE ////////
+// function keypad() {
+//     function keypadChange(event) {
+//         console.log(`${event.target}: ${event.target.value}`)
+//     }
+//     return(
+//         <div>
+//             <input
+//               tagName="INPUT"
+//               type="password"
+//               onChange={keypadChange}
+//               placeholder="Entering password..."
+//             />
+//         </div>
+//     )
+// }
+
+//////// SOLUTION ////////
+import { getElementsByTagName } from "domutils";
+import React from "react";
+
+function Keypad(){
+    function keypadChange(){
+        console.log("Entering password...")
+    }
+    return (
+        <input type="password" onChange={keypadChange} />
+    )
+}
+
+export default Keypad
+
+
+//////// NOTES ////////
+
+// 1. didn't put the import from React.... at the top
+// 2. capitalize the variable after the function 
+// 3. didn't put export default at the end 
+// 4. you're not suppose to add a parameter
+// 5. you basically copied the example from Canvas and didn't look back at the  steps
+// 6. you used tagName, type, and placeholder within the input when you only needed type
+
+
+// STEPS:
+// 1. In the components/Keypad.js file, create a Keypad React component.
+// 2. In that component, render an input with the right type.
+// 3. On that input, add an event handler that listens for the change event.
+// 4. When that event fires, use console.log to print out the text 'Entering password...'.
